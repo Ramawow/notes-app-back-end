868b0e33323271d5503e650e93b8a58dc7bd7c4e
ramawow <rmt0276@gmail.com>
2023-04-11 16:18:06 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,37 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-console */
+/* eslint-disable eol-last */
+/* eslint-disable indent */
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable linebreak-style */
+/* eslint-disable spaced-comment */
+/* eslint-disable linebreak-style */
+//console.log('Hallo kita akan membuat RESTful API');
+// run npm init --y
+// npm install nodemon --save-dev
+// npm install eslint --save-dev
+// npx eslint --init
+// eslint-disable-next-line linebreak-style
+// npm run lint
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 5000,
+        host: 'localhost',
+        routes: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    });
+
+    server.route(routes);
+
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
